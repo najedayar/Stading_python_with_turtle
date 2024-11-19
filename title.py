23b6eb45db2f0c88c00464570854cc3d16f0d073
najedayar <mongialouedayari@gmail.com>
2024-11-19 21:21:21 +0100

    modified:   title.py

--- title.py
+++ title.py
@@ -1,6 +1,54 @@
 from turtle import *
-
+############set viryable################
+x = -4
+y = 4
+#################set commands##################
+title("Project : Smart Cross Road")
+fillcolor("purple")
 speed(1)
-color()
+color("purple")
+begin_fill()
+goto(x, y)
+
+forward(100)
+right(90)
+forward(100)
+right(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(10)
+left(90)
+forward(100)
+right(90)
+forward(100)
+right(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(10)
+m = 100
+x=+ 100
+
+goto(x, y)
+left(90)
+forward(m)
+right(90)
+forward(10)
+left(90)
+forward(m)
+right(90)
+forward(10)
+left(90)
+forward(m)
+left(90)
+forward(m)
+
 
 
